bee739913d9e93c8a349e7fce16585434b9bf8d1
yudan <274920136@qq.com>
2025-03-01 17:19:07 +0800

    完成增删查

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,6 +1,10 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class MainMenu {
     static ArrayList<Bills> billsList = new ArrayList<>();
@@ -35,27 +39,60 @@ public class MainMenu {
         while(flag) {
             int opt = scanner.nextInt();
             switch (opt) {
-                case 1:
+                case 1 -> {
                     System.out.println("已选择：添加账务");
-                    break;
-                case 2:
+                    addBills();
+                }
+                case 2 -> {
                     System.out.println("已选择：删除账务");
-                    break;
-                case 3:
+                    deleteBills();
+                }
+                case 3 -> {
                     System.out.println("已选择：查询账务");
                     select();
-                    break;
-                case 4:
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("请重新输入：");
+                }
+                case 4 -> flag = false;
+                default -> System.out.println("请重新输入：");
             }
         }
         System.out.println("退出系统");
 
     }
+
+    private static void deleteBills() {
+        System.out.println("随手记>>删除账务");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要删除第几条账务");
+        int opt = scanner.nextInt();
+        billsList.remove(opt - 1);
+        showMenu();
+    }
+
+    private static void addBills() {
+        System.out.println("随手记>>添加账务");
+        Scanner scanner = new Scanner(System.in);
+        Bills bills = new Bills();
+        System.out.println("请输入账务类别(用途)：");
+        bills.setName(scanner.next());
+        System.out.println("请输入账户：");
+        bills.setAccount(scanner.next());
+        System.out.println("请输入类型：");
+        bills.setType(scanner.next());
+        System.out.println("请输入金额：");
+        bills.setTotal(scanner.nextDouble());
+        System.out.println("请输入时间：");
+        bills.setTime(scanner.next());
+        System.out.println("请输入备注：");
+        bills.setDesc(scanner.next());
+
+        billsList.add(bills);
+        System.out.println("添加账务成功！");
+        showMenu();
+    }
+
+    //查询账务
     public static void select() {
+        System.out.println("随手记>>账务查询");
         System.out.println("请选择您要查询的类型:");
         System.out.println("1.查询所有  2.按照时间区间查询  3.按照收入和支出的类型查询");
         Scanner scanner = new Scanner(System.in);
@@ -78,9 +115,41 @@ public class MainMenu {
     }
 
     private static void selectByType() {
+        System.out.println("随手记>>账务查询>>按照类型查询");
+        System.out.println("请输入查询类型：收入or支出");
+        Scanner scanner = new Scanner(System.in);
+        String type = scanner.next();
+        List<Bills> billsList1 = billsList.stream()
+                .filter(bills -> {
+                    String type1 = bills.getType();
+                    return type1.equals(type);
+        }).toList();
+        print(billsList1);
     }
 
     private static void selectByTime() {
+        System.out.println("随手记>>账务查询>>按照时间查询");
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("请输入开始时间：");
+        String start = scanner.next();
+        System.out.println("请输入结束时间：");
+        String end = scanner.next();
+        List<Bills> billsList1 = billsList.stream().filter(bills -> {
+            String time = bills.getTime();
+            try {
+                Date startDate = format.parse(start);
+                Date endDate = format.parse(end);
+                Date timeDate = format.parse(time);
+                if (timeDate.before(endDate) && timeDate.after(startDate)) {
+                    return true;
+                }
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            return false;
+        }).toList();
+        print(billsList1);
     }
 
     private static void selectAll() {
